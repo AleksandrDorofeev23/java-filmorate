c32c450b4c0ac5adc3cd2c85f9af0bb58f096bcc
AleksandrDorofeev23 <110825244+AleksandrDorofeev23@users.noreply.github.com>
2023-05-09 02:40:09 +0300

    1.4

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -8,12 +8,6 @@ CREATE TABLE  mpa (
 	id int PRIMARY KEY,
 	name varchar
 );
-DROP TABLE IF EXISTS film_genres CASCADE;
-CREATE TABLE film_genres (
-    film_id int REFERENCES films (film_id),
-    genre_id int REFERENCES genres (id),
-    film_genre_id int PRIMARY KEY AUTO_INCREMENT
-);
 DROP TABLE IF EXISTS films CASCADE;
 CREATE TABLE  films (
     film_id int PRIMARY KEY AUTO_INCREMENT,
@@ -23,11 +17,11 @@ CREATE TABLE  films (
     duration int,
     mpa_id int REFERENCES mpa (id)
 );
-DROP TABLE IF EXISTS likes CASCADE;
-CREATE TABLE  likes (
+DROP TABLE IF EXISTS film_genres CASCADE;
+CREATE TABLE film_genres (
     film_id int REFERENCES films (film_id),
-    user_id int REFERENCES users (user_id),
-    like_id int PRIMARY KEY AUTO_INCREMENT
+    genre_id int REFERENCES genres (id),
+    film_genre_id int PRIMARY KEY AUTO_INCREMENT
 );
 DROP TABLE IF EXISTS users CASCADE;
 CREATE TABLE  users (
@@ -36,6 +30,12 @@ CREATE TABLE  users (
     login varchar,
     name varchar,
     birthday date
+);
+DROP TABLE IF EXISTS likes CASCADE;
+CREATE TABLE  likes (
+    film_id int REFERENCES films (film_id),
+    user_id int REFERENCES users (user_id),
+    like_id int PRIMARY KEY AUTO_INCREMENT
 );
 DROP TABLE IF EXISTS friends CASCADE;
 CREATE TABLE  friends (
