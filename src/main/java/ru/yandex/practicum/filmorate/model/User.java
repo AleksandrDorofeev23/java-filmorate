5900600ad5a606eac4ae88abedd28b7715b417f8
AleksandrDorofeev23 <110825244+AleksandrDorofeev23@users.noreply.github.com>
2023-05-11 17:54:32 +0300

    2.1.1

--- src/main/java/ru/yandex/practicum/filmorate/model/User.java
+++ src/main/java/ru/yandex/practicum/filmorate/model/User.java
@@ -8,8 +8,6 @@ import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.PastOrPresent;
 import java.time.LocalDate;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.Set;
 
 @AllArgsConstructor
